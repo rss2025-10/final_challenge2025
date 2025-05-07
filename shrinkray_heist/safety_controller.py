3ae7b1993e64babad0343bc24ece84c4b318ee59
Julia Fortt <juliamfortt@gmail.com>
2025-05-07 15:23:46 -0400

    add extension to safety controller for use in either challenge module

--- shrinkray_heist/safety_controller.py
+++ shrinkray_heist/safety_controller.py
@@ -19,23 +19,40 @@ class SafetyController(Node):
 
         self.declare_parameter("safety_topic", "/vesc/low_level/input/safety")
         self.declare_parameter("scan_topic", "/scan")
+        self.declare_parameter("module", "shrinkray_heist")
 
         self.SCAN_TOPIC = self.get_parameter("scan_topic").get_parameter_value().string_value
         self.SAFETY_TOPIC = self.get_parameter("safety_topic").get_parameter_value().string_value
+        self.MODULE = self.get_parameter("module").get_parameter_value().string_value
 
         self.scan_subscriber = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.laser_scan_callback, 10)
         self.safety_publisher = self.create_publisher(AckermannDriveStamped, self.SAFETY_TOPIC, 10)
 
-        self.scan_length = None
-        self.max_speed = 1.5
-        self.speed = 1.5
+        if (self.MODULE == "shrinkray_heist"):
+            self.scan_length = None
+            self.max_speed = 1.5
+            self.speed = 1.5
+            self.HARD_STOP_BOUND = 0.7
+            self.SLOW_BOUND = 0.8
+        elif (self.MODULE == "race_to_the_moon"):
+            self.scan_length = None
+            self.max_speed = 1.5
+            self.speed = 1.5        
+            self.HARD_STOP_BOUND = 0.05
+            self.SLOW_BOUND = 0.1
+        else:
+            raise ValueError("Invalid module name in safety controller!")
 
     def laser_scan_callback(self, msg):
         # self.get_logger().info("Scanning")
         if not self.scan_length:
             self.scan_length = len(msg.ranges)
-            self.left_bound = self.scan_length // 6
-            self.right_bound = self.scan_length - self.scan_length // 6
+            if self.MODULE == "shrinkray_heist":
+                self.left_bound = self.scan_length // 6
+                self.right_bound = self.scan_length - self.scan_length // 6
+            elif self.MODULE == "race_to_the_moon":
+                self.left_bound = self.scan_length // 10
+                self.right_bound = self.scan_length - self.scan_length // 10
 
         scan = msg
         ranges = scan.ranges
@@ -43,16 +60,13 @@ class SafetyController(Node):
         # calculated range values
         use_range = range(self.left_bound, self.right_bound, 10)
 
-        HARD_STOP_BOUND = 0.7
-        # SLOW_BOUND = 0.8
-
         for k in use_range:
             chunk_end = min(k+10, len(ranges))
             chunk_values = [ranges[i] for i in range(k, chunk_end) if not np.isnan(ranges[i]) and not np.isinf(ranges[i])]
 
             if len(chunk_values) > 0:
                 average = sum(chunk_values) / len(chunk_values)
-                if average < HARD_STOP_BOUND:
+                if average < self.HARD_STOP_BOUND:
                     new_msg = AckermannDriveStamped()
                     header = Header()
                     header.stamp = self.get_clock().now().to_msg()
@@ -76,8 +90,6 @@ class SafetyController(Node):
         # self.get_logger().info("self.speed: " + str(self.speed))
         self.get_logger().info("self.angle: " + str(msg.drive.steering_angle))
         self.safety_publisher.publish(msg)
-
-
 
 
 def main():
